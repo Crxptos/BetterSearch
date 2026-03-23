b71905d807a0b8b44e3d760b30836f983d9e1df3
Crxptos <planeseven7@gmail.com>
2026-03-23 18:25:09 +0000

    Update BetterSearchMain.cpp

--- src/BetterSearchMain.cpp
+++ src/BetterSearchMain.cpp
@@ -1,6 +1,5 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/LevelSearchLayer.hpp>
-#include <Geode/utils/web.hpp>
 
 using namespace geode::prelude;
 
@@ -16,37 +15,21 @@ class $modify(BetterSearchMain, LevelSearchLayer) {
 
         log::info("BetterSearch query: {}", query);
 
-        // 🔍 Optional: Web request (can expand later)
-        std::string url = "https://gdbrowser.com/api/search/" + query;
+        // ✅ Fake results (safe, no web API)
+        std::vector<std::string> results = {
+            "Bloodbath",
+            "Bloodlust",
+            "Bloody",
+            "Future Funk",
+            "Sunset Sandstorm"
+        };
 
-        web::AsyncWebRequest()
-            .fetch(url)
-            .then([this, query](auto* res) {
-                log::info("Received response");
-
-                // 👉 For now: use dummy results
-                std::vector<std::string> results = {
-                    "Bloodbath",
-                    "Bloodlust",
-                    "Bloody",
-                    "Future Funk",
-                    "Sunset Sandstorm"
-                };
-
-                // 🔗 Send to UI (implemented in BetterSearchLevels.cpp)
-                this->updateBetterSearchUI(results);
-            })
-            .expect([](auto* err) {
-                log::error("Search failed");
-            });
+        // 🔗 Send to BetterSearchLevels UI
+        if (auto layer = typeinfo_cast<LevelSearchLayer*>(this)) {
+            static_cast<BetterSearchLevels*>(layer)->updateBetterSearchUI(results);
+        }
 
         // Keep original GD search
         LevelSearchLayer::onSearch(sender);
-    }
-
-    // 🔗 This connects to your UI system
-    void updateBetterSearchUI(std::vector<std::string> results) {
-        // This function will be defined in BetterSearchLevels.cpp
-        // (same modified class)
     }
 };
